1df84f2b0e886f9b8eaba34c43c9b65050d84504
Alex Polson <apolson@amazon.com>
2012-02-04 17:23:50 -0800

    Adding first and last name to authors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120205003208) do
+ActiveRecord::Schema.define(:version => 20120205005557) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(:version => 20120205003208) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
