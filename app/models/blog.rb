cd43d4659fa87bbe31d1201ae098c0fddc29a88e
Alex Polson <apolson@amazon.com>
2012-02-24 10:25:24 -0800

    Only show those articles that aren't draft articles.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,12 +2,8 @@ class Blog < ActiveRecord::Base
   has_many :blog_articles
   has_many :articles, through: :blog_articles, order: 'created_at DESC'
   
-  def finalized_articles(current_author_id)
-    # TODO: Adjust this such that the logged in author can see their draft posts too.
-    self.articles.where(
-      "is_draft = false or is_draft is null or author_id = :author_id",
-      { author_id: current_author_id }
-    )
+  def finalized_articles
+    self.articles.where('is_draft = false')
   end
   
 end
